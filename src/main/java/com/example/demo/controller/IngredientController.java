df1a120a9e425876a9f07053893a17e4c780c390
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2020-11-27 17:22:39 +0100

    Add CrossOrigin annotation on controllers

--- src/main/java/com/example/demo/controller/IngredientController.java
+++ src/main/java/com/example/demo/controller/IngredientController.java
@@ -1,10 +1,27 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@CrossOrigin(origins = {
+        "http://localhost:8081",
+        "http://localhost:3000",
+        "https://wbyc-web-client.herokuapp.com"
+})
 @RestController
 @RequestMapping("/ingredients")
 public class IngredientController {
 
+    @GetMapping({"/", ""})
+    public ResponseEntity<Map<String, Object>> getPageOfRecipes(
+            @RequestParam(required = false) String key,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size,
+            @RequestParam(defaultValue = "id,asc") String[] sort) {
+
+        System.out.println("w/e");
+        return null;
+    }
 }
--- src/main/java/com/example/demo/controller/RecipeController.java
+++ src/main/java/com/example/demo/controller/RecipeController.java
@@ -3,9 +3,16 @@ package com.example.demo.controller;
 import com.example.demo.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@CrossOrigin(origins = {
+        "http://localhost:8081",
+        "http://localhost:3000",
+        "https://wbyc-web-client.herokuapp.com"
+})
+@RestController
 @RequestMapping("/recipes")
 public class RecipeController {
     @Autowired
